73a0a767ddd66b69d94b0753e942b2242ddcbb53
weedge <weege007@gmail.com>
2025-06-20 09:28:54 +0800

    fix version read
    Signed-off-by: weedge <weege007@gmail.com>

--- funasr_local/__init__.py
+++ funasr_local/__init__.py
@@ -4,5 +4,8 @@ import os
 
 dirname = os.path.dirname(__file__)
 version_file = os.path.join(dirname, "version.txt")
-with open(version_file, "r") as f:
-    __version__ = f.read().strip()
+if os.path.exists(version_file):
+    with open(version_file, "r") as f:
+        __version__ = f.read().strip()
+else:
+    __version__ = "0.4.4"
